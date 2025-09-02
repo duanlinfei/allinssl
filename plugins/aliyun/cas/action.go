78701b99374b5a19129e0280ebd0625999776de1
karotto <kaMbp@gmail.com>
2025-09-02 14:09:02 +0800

    cas优化

--- plugins/aliyun/cas/action.go
+++ plugins/aliyun/cas/action.go
@@ -1,6 +1,9 @@
 package cas
 
 import (
+	"crypto/md5"
+	"fmt"
+
 	cas "github.com/alibabacloud-go/cas-20200407/v4/client"
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
@@ -20,12 +23,25 @@ func CreateClient(accessKey, accessSecret, endpoint string) (*cas.Client, error)
 }
 
 func UploadToCas(client *cas.Client, cert, key, name string) error {
+	suffix := GetMD5Suffix(cert)
 	uploadUserCertificateRequest := &cas.UploadUserCertificateRequest{
-		Name: tea.String(name),
+		Name: tea.String(name + suffix),
 		Cert: tea.String(cert),
 		Key:  tea.String(key),
 	}
 	runtime := &util.RuntimeOptions{}
 	_, err := client.UploadUserCertificateWithOptions(uploadUserCertificateRequest, runtime)
 	return err
+}
+
+func GetMD5Suffix(data string) string {
+	// 计算MD5值
+	hash := md5.Sum([]byte(data))
+	// 转换为十六进制字符串
+	md5Str := fmt.Sprintf("%x", hash)
+	// 获取后6位
+	if len(md5Str) > 6 {
+		return md5Str[len(md5Str)-6:]
+	}
+	return md5Str
 }
